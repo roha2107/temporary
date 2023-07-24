d08949248a902dfff612cf6e6f9d450d45190d5b
Rakhima Muratova <mroha889@mail.ru>
2023-07-24 16:44:59 +0600

    first commit

---
+++ src/Figure.java
@@ -0,0 +1,48 @@
+public class Figure {
+
+}
+class Triangle extends Figure{
+
+}
+class IsoscelesTriangle extends Triangle{
+
+}
+class EquilateralTriangle extends Triangle{
+
+}
+class RightTriangle extends Triangle{
+
+}
+class Quadrangle extends Figure{
+
+}
+class ConvexQuadrilateral extends Quadrangle{
+
+}
+class Parallelogram extends Quadrangle{
+
+}
+class Rectangle extends Quadrangle{
+
+}
+class Square extends Quadrangle{
+
+}
+class Rhombus extends Quadrangle{
+
+}
+class Trapeze extends Quadrangle{
+
+}
+class Pentagon extends Figure{
+
+}
+class Hexagon extends Figure{
+
+}
+class Oval extends Figure{
+
+}
+class Circle extends Figure{
+
+}
